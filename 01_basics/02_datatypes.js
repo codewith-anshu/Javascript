caad04288a9d0ee3ff8d1d99e9d7df6c14ba364e
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-17 06:23:52 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all Js code as a new version
+//alert (3+3) // we are using nodejs not browser
+
+console.log (3 
+    + 3) // code readability should be high
+    
+    console.log ("anshu")
+
+    let name = "anshu"
+    let age = 18
+    let isLoggedIn = true
+    let state;
+
+    //PREMATIVE DATATYPES
+    // number=> 2 to the power 53
+    // bigint // for more data above power of 2 to the 53
+    // string => ""
+    //boolean => true/false
+    //null=> standalone value
+    // undefined =>
+    // symbol => unique
+
+    //object
+
+    console.log (typeof null) // output is object
+    console.log (typeof undefined) // output is undefined
