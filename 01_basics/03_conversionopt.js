bd30905c38b1c3d67831ff2981aa01473468e0ee
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-19 09:09:50 +0000

    Conversion

---
+++ 01_basics/03_conversionopt.js
@@ -0,0 +1,27 @@
+let score = "anshu"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NAN
+// true =>1; false => 0
+
+let isLoggedIn = "anshu"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "anshu" => true
+ 
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
