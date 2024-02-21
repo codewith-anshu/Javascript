ae09075bb21ee44e1a519a1ae2e1736466ef5e7c
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-21 08:29:30 +0000

    num and math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,15 @@
+ const score = 400 
+ console.log(score)
+
+ const balance = new Number (100)
+ console.log(balance);
+
+ console.log(balance.toString().length)
+ console.log(balance.toFixed(1))
+
+ const otherNumber = 123.6678
+
+ console.log (otherNumber.toPrecision(4))
+ const hundreds = 100000
+ console.log(hundreds.toLocaleString('en-In'))
+
