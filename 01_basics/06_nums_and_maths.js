87b775fed594c7f74db7625d7cdf7b0fea567e22
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-24 14:59:20 +0000

    math and num

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,5 +1,5 @@
  const score = 400 
- console.log(score)
+ /*console.log(score)
 
  const balance = new Number (100)
  console.log(balance);
@@ -11,5 +11,26 @@
 
  console.log (otherNumber.toPrecision(4))
  const hundreds = 100000
- console.log(hundreds.toLocaleString('en-In'))
+ console.log(hundreds.toLocaleString('en-In'))*/
+
+ // ########### MATHS ************* ////
+
+ /*console.log(Math);
+ console.log(Math.abs(-4));
+ console.log(Math.round(4.6))
+ console.log(Math.ceil(4.2)) //// ceil is used for chossing top values 
+console.log(Math.floor(5.3)) // // choose down value 
+console.log(Math.min(5.3))
+console.log(Math.max(5.3))*/
+
+
+// math.random values always lies b/w 0 to 1
+console.log(Math.random()) 
+console.log((Math.random()*10) + 1)
+
+const min = 10
+const max = 30
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min) 
+
 
