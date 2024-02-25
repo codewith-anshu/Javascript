5a467e053cdcdd0ee9fba7ae7d688b724fa3d624
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-25 05:33:12 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,6 +1,6 @@
 // array
 
-const myArr = [ 0 , 1, 2, 3, 4, 5]
+/*const myArr = [ 0 , 1, 2, 3, 4, 5]
 const myHeros = ["TMKOC", "Ninja"]
 
 const myArr2 = new Array (1 , 2 , 3 , 4)
@@ -18,7 +18,7 @@ myArr.pop()*/
 
 /*const newArr = myArr.join()
 console.log(myArr)
-console.log(newArr)*/
+console.log(newArr)
 
 // *****  slice , splice ******
 
@@ -31,7 +31,40 @@ console.log("B ", myArr)
 
 const myn2 = myArr.splice(1, 3)
 console.log("C ", myArr)
-console.log( myn2)
+console.log( myn2)*/
+
+////// Part 2 ----ARray //////
+
+const marvel_heros = ["thor", "Ironman" , "spiderman"]
+const dc_heros = ["superman", "flash", "batman"]
+
+//marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros)
+//console.log(marvel_heros[3][1])
+
+//const allHeros = marvel_heros.concat(dc_heros)
+//console.log(allHeros)
+
+const all_new_heros = [...marvel_heros,...dc_heros] // insted of concat we use "spread"
+//console.log(all_new_heros)
+
+const another_array = [ 1, 2, 3, [4, 5, 6], 7,[6, 7,[4 , 5]]]
+
+const real_another_array = another_array.flat(Infinity)
+console.log(real_another_array)
+
+
+console.log(Array.isArray("anshu"))
+console.log(Array.from("anshu"))
+console.log(Array.from({name: "anshu"})) // interasting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3))
+
 
 
 
