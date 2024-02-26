54f9f66d94a3010d3af244fa50d43ecbbfa2ead8
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-26 17:15:09 +0000

    scope

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -5,7 +5,7 @@ if (true){ // inside if "block scope "
     let a = 10
     const b = 20
     //var c = 30
-    console.log("INNER: ", a) // print global inside block
+    //console.log("INNER: ", a) // print global inside block
 }
 // outside if global scope , global scope value available inside block scope but block scope dont go outside of "if"
 /*for (let i  = 0; i < array.length; i++) {
@@ -15,6 +15,42 @@ if (true){ // inside if "block scope "
 
 //console.log(a);
 //console.log(b);
-console.log(a); 
+//console.log(a); 
 
 //////PART 2 -> Scope and all/
+
+function one(){
+    const username = "anshu"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+//one()
+/// when nested function happens chlid function access parents variable.\
+if (true){
+    const username = "anshu"
+    if (username === "anshu"){
+        const website = "youtube"
+        //console.log(useranme + website );
+    
+}
+    // comsole.log(website)
+       
+}
+// console.log(username);
+
+
+//////// +++++ interesting ----////>>>>
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+addTwo(5) // hoisting method  that why we get error.
+const addTwo = function(num){
+    return num + 2
+}
