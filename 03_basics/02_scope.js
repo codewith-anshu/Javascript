d4132313ae711977669df315361f8923166fffcd
Anshu Kumar <118442401+codewith-anshu@users.noreply.github.com>
2024-02-26 08:29:50 +0000

    scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,18 @@
+//var c = 300
+let a = 300
+
+if (true){ // inside if "block scope "
+    let a = 10
+    const b = 20
+    //var c = 30
+    console.log("INNER: ", a) // print global inside block
+}
+// outside if global scope , global scope value available inside block scope but block scope dont go outside of "if"
+/*for (let i  = 0; i < array.length; i++) {
+    const element = array[i];
+    
+}*/
+
+//console.log(a);
+//console.log(b);
+console.log(a);
